961977d34119bb0a00ef35cd9a4af02648630b77
Vinícius Quadrado <viniciusquare44@gmail.com>
2023-06-09 17:56:52 -0300

    draft/removing aws to ease prisma transition

--- src/aws/aws.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { AWSReportService } from "./services/awsReport.service";
-import { AWSReportController } from "./awsReport.controller";
-
-@Module({
-    providers: [ AWSReportService ],
-    controllers: [ AWSReportController ],
-})
-export class AWSModule {}
--- src/aws/services/awsReport.service.ts
+++
@@ -1,6 +0,0 @@
-export class AWSReportService {
-
-  constructor() { }
-
-}
-
